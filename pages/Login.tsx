c83a08d6cb999fb2b4c3d1e8d818d030eb492af1
Marco Bonafini <marcobonafini@Marcos-MacBook-Pro.local>
2023-09-28 20:05:35 -0500

    minor fixes

--- pages/Login.tsx
+++ pages/Login.tsx
@@ -1,9 +1,17 @@
 import "../app/globals.css";
+import Image from "next/image";
 
 const Login = () => {
   return (
     <div className="flex items-center justify-center min-h-screen">
       <h1 className="text-orange-500 text-3xl">Login</h1>
+      <Image
+        className="rounded-3xl mb-5"
+        src="/bn.png"
+        alt="Description of Image"
+        width={2000}
+        height={2000}
+      />
     </div>
   );
 };
